4d084ba8e70fa668b5a222c07b79c22ec2a669e8
Petr Janda <petrjandacz@gmail.com>
2015-08-10 10:57:01 +0100

    remove launch and move everything to op

--- op.py
+++ op.py
@@ -1,10 +1,10 @@
-import os, sys
-import argparse
+import os, sys, argparse, botocore, utils, logging
 import lib.env as env
-import utils
 
 from lib.cluster import Cluster
 from lib.cluster_launcher import ClusterLauncher
+from lib.cloud_config import CloudConfig
+from lib.coreos import Ami as CoreOSAmi
 
 env.check()
 
@@ -17,11 +17,47 @@ parser_scp.add_argument('key_pair_path')
 parser_scp.add_argument('from_path')
 parser_scp.add_argument('to_path')
 
+parser_launch = subparsers.add_parser('launch')
+parser_launch.add_argument("instance_type")
+parser_launch.add_argument("instances_count")
+parser_launch.add_argument("key_pair_name")
+parser_launch.add_argument("cloud_config_path")
+
 args = parser.parse_args()
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+region = os.getenv("AWS_DEFAULT_REGION")
 cluster = Cluster(args.cluster_name)
 
 if args.op == "dns":
     print([i.public_dns_name for i in cluster.instances])
+
+elif args.op == 'launch':
+    logging.info("--> Fetching CoreOS etcd discovery token")
+    cloud_config = CloudConfig(
+        open(args.cloud_config_path).read()
+    ).with_new_token(args.instances_count)
+
+    try:
+        launcher = ClusterLauncher()
+        ami = CoreOSAmi.get_ami(region, args.instance_type)
+
+        launcher.launch(
+            args.cluster_name, 
+            cloud_config, 
+            ami,
+            args.key_pair_name,
+            count = int(args.instances_count),
+            instance_type = args.instance_type
+        )
+
+        instances = Cluster(args.cluster_name).instances
+
+        logging.info("--> " + str([i.public_dns_name for i in instances]))
+    except botocore.exceptions.WaiterError:
+        logging.error("--x Failed to launch instances, Please check your AWS console, some machines may be already running!") 
+    finally:
+        cluster.cleanup()
 
 elif args.op == "status":
     print(cluster.status)
