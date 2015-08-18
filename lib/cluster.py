1587f79127b875dd8cb7cf67984d6435dc971222
Petr Janda <petrjandacz@gmail.com>
2015-08-18 20:46:00 +0100

    more linting fixes

--- lib/cluster.py
+++ lib/cluster.py
@@ -1,10 +1,12 @@
+""" CoreOS Cluster """
+
 import boto3 as aws
 import itertools
 import botocore
 import logging
 
 class Cluster:
-    """ CoreOS cluster """
+    """ CoreOS Cluster """
 
     def __init__(self, name):
         self.name = name
@@ -12,6 +14,7 @@ class Cluster:
 
     @property
     def instances(self):
+        """ List of instances within the cluster """
         return self.ec2.instances.filter(
             Filters=[
                 {'Name': 'tag-key', 'Values': ['Cluster']},
@@ -21,44 +24,56 @@ class Cluster:
 
     @property
     def status(self):
+        """ Overall status of cluster instances """
         statuses = set([i.state['Name'] for i in list(self.instances)])
 
-        if(len(statuses) > 1):
+        if len(statuses) > 1:
             raise Exception("Cluster in inconsistent state '%s'!" % statuses)
 
         return list(statuses)[0]
 
     def terminate(self):
+        """ Terminate the cluster """
+
         logging.info('--> Terminate instances')
         self.instances.terminate()
         for instance in self.instances:
             instance.wait_until_terminated()
 
     def stop(self):
+        """ Stop the cluster """
+
         logging.info('--> Stop instances')
         self.instances.stop()
         for instance in self.instances:
             instance.wait_until_stopped()
 
     def start(self):
+        """ Start the cluster """
+
         logging.info('--> Start instances')
         self.instances.start()
         for instance in self.instances:
             instance.wait_until_running()
 
     def cleanup(self):
+        """ Cleanup and terminate the cluster (remove security groups) """
+
         logging.info("--> Delete security groups")
-        groups = set([g['GroupId'] for g in itertools.chain(*map(lambda i: i.security_groups, self.instances))])
-        
+        instance_security_groups = map(lambda i: i.security_groups, self.instances)
+        groups = set([g['GroupId'] for g in itertools.chain(*instance_security_groups)])
+
         self.terminate()
 
-        for g in groups:
+        for group in groups:
             try:
-                self.ec2.SecurityGroup(g).delete()
+                self.ec2.SecurityGroup(group).delete()
 
-                logging.info("--> " + g + " deleted")
-            except botocore.exceptions.ClientError as e:
-                logging.info("--> " + g + " couldn't be deleted")
-      
+                logging.info("--> " + group + " deleted")
+            except botocore.exceptions.ClientError:
+                logging.info("--> " + group + " couldn't be deleted")
+
     def exists(self):
+        """ Check for cluster existence """
+
         return len(list(self.instances)) > 0
--- lib/cluster_conf.py
+++ lib/cluster_conf.py
@@ -1,10 +1,18 @@
+""" ClusterConf """
+
+#TODO reduce properties
 class ClusterConf:
-    def __init__(self, cluster_name, ami, key_pair_name, user_data = '', instance_type = 'm1.small', instances_count = 1, allocate_ip_address = False):
+    """ ClusterConf """
+
+    #TODO reduce attributes
+    def __init__(self, cluster_name, ami, key_pair_name,
+                 user_data='', instance_type='m1.small',
+                 instances_count=1, allocate_ip_address=False):
         self._cluster_name = cluster_name
         self._ami = ami
         self._user_data = user_data
         self._key_pair_name = key_pair_name
-        self._instance_type = instance_type 
+        self._instance_type = instance_type
         self._instances_count = instances_count
         self._allocate_ip_address = allocate_ip_address
         self._volumes = []
@@ -12,67 +20,89 @@ class ClusterConf:
 
     @property
     def ami(self):
+        """ Ami """
+
         return self._ami
 
     @property
     def security_groups(self):
+        """ List of security groups """
+
         return self._security_groups
 
     @property
     def key_pair_name(self):
+        """ Key pair name used to access the cluster """
+
         return self._key_pair_name
 
     @property
     def cluster_name(self):
+        """ Name """
+
         return self._cluster_name
 
     @property
     def instance_type(self):
+        """ EC2 Instance Type """
+
         return self._instance_type
 
     @property
     def instances_count(self):
+        """ Instances count """
+
         return self._instances_count
 
     @property
     def allocate_ip_address(self):
+        """ Allocate elastic IP address for the cluster nodes? """
+
         return self._allocate_ip_address
 
     @property
     def block_device_mappings(self):
+        """ Block device mapping for EC2 instance creation """
+
         return [dict(
-            DeviceName = v['name'],
-            Ebs = dict(
-                VolumeSize = v['size'],
-                VolumeType = v['volume_type'],
-                DeleteOnTermination = v['delete_on_termination']
+            DeviceName=v['name'],
+            Ebs=dict(
+                VolumeSize=v['size'],
+                VolumeType=v['volume_type'],
+                DeleteOnTermination=v['delete_on_termination']
             )
         ) for v in self._volumes]
 
     @property
     def props(self):
+        """ Props for EC2 cluster creation """
+
         return dict(
-            ImageId = self._ami, 
-            UserData = self._user_data,
-            MinCount = 1, 
-            MaxCount = self.instances_count,
-            KeyName = self.key_pair_name,
-            InstanceType = self.instance_type,
-            Monitoring = dict(
-                Enabled = True
+            ImageId=self._ami,
+            UserData=self._user_data,
+            MinCount=1,
+            MaxCount=self.instances_count,
+            KeyName=self.key_pair_name,
+            InstanceType=self.instance_type,
+            Monitoring=dict(
+                Enabled=True
             ),
 
-            BlockDeviceMappings = self.block_device_mappings
+            BlockDeviceMappings=self.block_device_mappings
         )
 
     #def volume(name = '/dev/sdb', size = 100, volume_type = 'gp2', delete_on_termination = True):
     def volume(self, **kwargs):
+        """ Add volume """
+
         # name, size, volume_type required
         self._volumes.append(kwargs)
 
         return self
 
     def security_group(self, **kwargs):
+        """ Add security group """
+
         self._security_groups.append(kwargs)
 
         return self
